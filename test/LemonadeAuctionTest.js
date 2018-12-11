bf2754b351dab75c70c1da30adfd74b4b48839fa
HTMLtag <jonathansoylandlier@gmail.com>
2018-12-11 20:26:38 +0100

    Created client app

--- test/LemonadeAuctionTest.js
+++ test/LemonadeAuctionTest.js
@@ -5,28 +5,28 @@ contract("Auction", accounts => {
   describe("createAuction", () => {
   
     it("Should create new auction", async () => {
-        let auctionContract = await LemonadeAuction.deployed();
+        let instance = await LemonadeAuction.deployed();
   
-        await auctionContract.mint("Green Lemonade", 156);
+        await instance.mint("Green Lemonade", 156);
 
-        await auctionContract.createAuction(0, 100);  
-        const auction = await auctionContract.lemonadeIdToAuction(0);
+        await instance.createAuction(0, 100);  
+        const auction = await instance.lemonadeIdToAuction(0);
         assert.equal(auction[0], accounts[0]);
         assert.equal(auction[1].toNumber(), 100);
     });
 
     it("Should only let owner create new auction for own Lemonade", async () => {
-        let auctionContract = await LemonadeAuction.deployed();
+        let instance = await LemonadeAuction.deployed();
   
-        await auctionContract.mint("Pink Lemonade", 146);
+        await instance.mint("Pink Lemonade", 146);
         let other = accounts[1];
       
         //transfer lemonade from owner to another
-        await auctionContract.safeTransferFrom(accounts[0], other, 1);
+        await instance.safeTransferFrom(accounts[0], other, 1);
 
         try{
             //Should not be able to create auction
-            await auctionContract.createAuction(1, 100);  
+            await instance.createAuction(1, 100);  
         }catch(error) {
             assert.equal(error, "Error: VM Exception while processing transaction: revert");
         }
@@ -36,18 +36,9 @@ contract("Auction", accounts => {
 
   describe("cancelAuction", () => { 
     it("Should cancel auction", async () => {
-        let auctionContract = await LemonadeAuction.deployed();
-        await auctionContract.cancel(0); 
-        const auction = await auctionContract.lemonadeIdToAuction(0); 
-        assert.equal(auction[1].toNumber(), 0);
-    });
-  });
-
-  describe("BidAuction", () => { 
-    it("Should be able to by lemonade", async () => {
-        let auctionContract = await LemonadeAuction.deployed();
-        await auctionContract.cancel(0); 
-        const auction = await auctionContract.lemonadeIdToAuction(0); 
+        let instance = await LemonadeAuction.deployed();
+        await instance.cancel(0); 
+        const auction = await instance.lemonadeIdToAuction(0); 
         assert.equal(auction[1].toNumber(), 0);
     });
   });

b99577e9dce37df6368d08b6d483a925be50ce0a
HTMLtag <jonathansoylandlier@gmail.com>
2018-12-16 04:27:27 +0100

    Updates

---
+++ precache-manifest.f2443919dc3b5c51bba6a4e282ef639f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/htmltag/freshLemonade/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "10c084c5f5c548751a48",
+    "url": "/htmltag/freshLemonade/static/js/runtime~main.10c084c5.js"
+  },
+  {
+    "revision": "7b11d27f13e748babcdb",
+    "url": "/htmltag/freshLemonade/static/js/main.7b11d27f.chunk.js"
+  },
+  {
+    "revision": "5e3527c23099f44c739a",
+    "url": "/htmltag/freshLemonade/static/js/1.5e3527c2.chunk.js"
+  },
+  {
+    "revision": "7b11d27f13e748babcdb",
+    "url": "/htmltag/freshLemonade/static/css/main.d4ebbfd9.chunk.css"
+  },
+  {
+    "revision": "f866766d098ca1e4a8afac0c99f65919",
+    "url": "/htmltag/freshLemonade/index.html"
+  }
+];
